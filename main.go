4a52266f74448be1484348f0188197d850b73bea
brandhawa99 <baltej.randhawa0@gmail.com>
2024-06-30 21:44:43 -0700

    created todos

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("HELLO WORld")
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "Hello, world"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := Todo{}
 	})
 
 	log.Fatal(app.Listen(":4000"))
